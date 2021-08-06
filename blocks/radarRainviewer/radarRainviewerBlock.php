3b0c6f2076b9672334dbc954ddf53b2484a69eef
Daali <daali@daali.net>
2021-08-06 11:33:02 -0400

    cleaned up some versioning in txt

--- blocks/radarRainviewer/radarRainviewerBlock.php
+++ blocks/radarRainviewer/radarRainviewerBlock.php
@@ -1,16 +1,11 @@
 <?php
 
-	# 		Name: radarBE
-	# 		Namespace:		radarBE
+	# 		Name: Radar Rainviewer 
+	# 		Namespace:		radarRainviewer
 	#		Meteotemplate Block
 
-	# 		v1.0 - Jul 3, 2017
-	# 			- initial release, thanks to Gianni for his help
-	#		v1.1 - Aug 30, 2017
-	# 			- compatibility fixes
-	#       v1.2 - Aug 6, 2021
-	#           - changed source from ajax to rainviewer iframe due to ajax source offline
-	
+	# 		v1.0 - Aug 6, 2021
+		
 		
 	// load theme
 	$designTheme = json_decode(file_get_contents("../../css/theme.txt"),true);
@@ -26,6 +21,5 @@
 	$language[$lang] = json_decode($languageRaw,true);
 	
 ?>
-	
-<iframe src="https://www.rainviewer.com/map.html?loc=50.6703,4.3421,5&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=1&rmt=4&c=1&o=83&lm=0&th=0&sm=1&sn=1" width="98%" frameborder="0" style="border:0;height:50vh;"></iframe>
+<iframe src="https://www.rainviewer.com/map.html?loc=<?php echo $stationLat?>,<?php echo $stationLon?>,5&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=1&rmt=4&c=1&o=83&lm=0&th=0&sm=1&sn=1" width="98%" frameborder="0" style="border:0;height:50vh;"></iframe>
 
